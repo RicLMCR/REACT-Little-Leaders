cb9bfaaebd13de39e90400fd390b1059dd05e983
Richard <isit_ric@hotmail.com>
2022-06-01 17:22:02 +0100

    commit 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Little Leaders V2</h1>
+      <p>Hannah Joynson & Richard Larson</p>
+    </div>
+  );
+}
+
+export default App;
