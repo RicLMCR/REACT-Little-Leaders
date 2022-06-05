ebb83ba92878d808983bbef0f5caaedcd9fa619d
Richard <isit_ric@hotmail.com>
2022-06-05 21:21:37 +0100

    commit 3

--- src/Char.js
+++ src/Char.js
@@ -1,23 +1,11 @@
-import {useState } from "react";
-
-const Char = () => {
-    // Character class object and sub-class here
-const [character, setCharacter] = useState ([]);
-
-// Function to return character object/class
-const generateChar = (name, narc, insec, controv)=>{
-    let charClass = [name, narc, insec, controv];
-    setCharacter(charClass);
-    return (
-        console.log(charClass)
-    )
-};
+const Char = ({title,narc,insec}) => {
 
     return ( 
-        <div>
-            <h1>Character Here</h1>
-            <button onClick={()=>{generateChar("The Donald", 10, 0, 0)}}>Generate Character</button>
-            <p>{character}</p>
+        <div className="charStats" >
+            <h1>Character Stats Here</h1>
+             <p>Name:{title}</p>
+             <p>Narcissm:{narc}</p>
+             <p>Insecurity:{insec}</p>
         </div>
      );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,18 @@
+
+
 const Nav = () => {
-    // Navigation and game reset button here
+
+
+
+
+
     return (  
         <div>
             <h1>Navigation Bar Here</h1>
+            {/* <ul>
+                <li><Char /></li>
+            </ul> */}
+            
         </div>
     );
 }
